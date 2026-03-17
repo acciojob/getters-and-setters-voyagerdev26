5f3fdba1474da64a56e2620041af509c741b5772
Dev Sharma <103065378+voyagerdev26@users.noreply.github.com>
2026-03-17 16:36:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 //complete this code
 class Person {
 	constructor(name,age){
-		this.name=name;
-		this.age=age;
+		this._name=name;
+		this._age=age;
 	}
 	get name(){
-		return this.name;
+		return this._name;
+	}
+	get age(){
+		return this._age;
 	}
 	set age(a){
-		this.age=a;
+		this._age=a;
 	}
 }
 
@@ -17,7 +20,7 @@ class Student extends Person {
 		super(name,age);
 	}
 	study(){
-		return `${this.name} is studying`;
+		console.log(`${this._name} is studying`);
 	}
 	
 }
@@ -27,7 +30,7 @@ class Teacher extends Person {
 		super(name,age);
 	}
 	teach(){
-		return `${this.name} is teaching`
+		console.log(`${this._name} is teaching`)
 	}
 }
 
