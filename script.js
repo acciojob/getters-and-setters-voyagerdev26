7311c0b47a900fbbac7782ecab0aedbfece5939a
Dev Sharma <103065378+voyagerdev26@users.noreply.github.com>
2026-03-17 15:59:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
+	get name(){
+		return this.name;
+	}
+	set age(a){
+		this.age=a;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	constructor(name,age){
+		super(name,age);
+	}
+	study(){
+		return `${this.name} is studying`;
+	}
+	
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	constructor(name,age){
+		super(name,age);
+	}
+	teach(){
+		return `${this.name} is teaching`
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
